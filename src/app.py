59372e29a3d7cdfe6a72aabff207ba94bc3914ac
Patricia Johansson <pj222uc@student.lnu.se>
2025-02-07 14:49:59 +0100

    Add starting point

---
+++ src/app.py
@@ -0,0 +1,12 @@
+# The starting point of the application.
+
+from SubstitutionCipher import SubstitutionCipher
+
+# option1Answer = input('Do you want to encrypt (E) or decrypt (D)?')
+
+# option2Answer = input('Do you want to use substitution (S) or transposition (T)?')
+
+plainText = input("Enter plaintext message:")
+
+cipher = SubstitutionCipher()
+print(cipher.encrypt(plainText, 123))
