02fc3cfe523afc7dee28bdcbd1d11dcd028c998b
Patricia Johansson <pj222uc@student.lnu.se>
2025-02-08 16:23:40 +0100

    Add decrypt method

--- src/SubstitutionCipher.py
+++ src/SubstitutionCipher.py
@@ -5,19 +5,9 @@ class SubstitutionCipher:
   standard_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
   def encrypt (self, plain_text, key):
-    cipher_key = self.get_cipher_alphabet(key)
-    # The encrypted letters will be added to a list.
-    cipher_text = []
+    cipher_alphabet = self.get_cipher_alphabet(key)
 
-    for letter in plain_text:
-      # Find the index of the letter in the standard alphabet,
-      # and replace it with the cipher Alphabet.
-      index = self.standard_alphabet.find(letter.upper())
-      if (index != -1):
-        cipher_text.append(cipher_key[index])
-      else:
-        cipher_text.append(letter.upper())
-    return "".join(cipher_text)
+    return self.substitute_text(plain_text, self.standard_alphabet, cipher_alphabet)
 
   def get_cipher_alphabet (self, key):
     # Use the key as a seed when shuffling the alphabet.
@@ -25,3 +15,25 @@ class SubstitutionCipher:
     alphabet_to_shuffle = list(copy.deepcopy(self.standard_alphabet))
     random.shuffle(alphabet_to_shuffle)
     return "".join(alphabet_to_shuffle)
+
+  def substitute_text (self, origin_text, origin_alphabet, substitute_alphabet):
+    # The encrypted letters will be added to a list.
+    substituted_text = []
+
+    for letter in origin_text:
+      # Find the index of the letter in the standard alphabet,
+      # and replace it with the cipher Alphabet.
+      index = origin_alphabet.find(letter.upper())
+      if (index != -1):
+        substituted_text.append(substitute_alphabet[index])
+      else:
+        substituted_text.append(letter.upper())
+    return "".join(substituted_text)
+
+  def decrypt (self, cipher_text):
+    return self.decrypt_with_key(cipher_text, 120)
+
+  def decrypt_with_key (self, cipher_text, key):
+    cipher_alphabet = self.get_cipher_alphabet(key)
+
+    return self.substitute_text(cipher_text, cipher_alphabet, self.standard_alphabet)
--- src/app.py
+++ src/app.py
@@ -2,11 +2,29 @@
 
 from SubstitutionCipher import SubstitutionCipher
 
-# option1Answer = input('Do you want to encrypt (E) or decrypt (D)?')
-
 # option2Answer = input('Do you want to use substitution (S) or transposition (T)?')
 
-plain_text = input("Enter plaintext message: ")
+try:
+  option1_answer = input("Do you want to encrypt (E) or decrypt (D)? ")
 
-cipher = SubstitutionCipher()
-print(cipher.encrypt(plain_text, 123))
+  if (option1_answer.upper() == "E"):
+    plain_text = input("Enter plaintext message: ")
+    key = input("Enter the secret key: ")
+
+    cipher = SubstitutionCipher()
+    print(cipher.encrypt(plain_text, key))
+  else:
+    cipher_text = input("Enter text to decipher: ")
+    key_known_answer = input("Is the key known? Yes (Y) No (N) ")
+    if (key_known_answer.upper() == "Y"):
+      key = input("Enter the key: ")
+
+      cipher = SubstitutionCipher()
+      print(cipher.decrypt_with_key(cipher_text, key))
+    else:
+      cipher = SubstitutionCipher()
+      print(cipher.decrypt(cipher_text))
+except TypeError as t:
+  print(t)
+except Exception as e:
+  print(e)
